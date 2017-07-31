0c1fc090a681cf8672e217c23f1f3a049d9b9552
SeeKT <seekt@acd.tarc.edu.my>
2017-07-31 13:46:29 +0800

    Completed code.

--- app/src/main/java/my/edu/tarc/lab43sqlite/AddActivity.java
+++ app/src/main/java/my/edu/tarc/lab43sqlite/AddActivity.java
@@ -2,6 +2,8 @@ package my.edu.tarc.lab43sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
     }
+
+    public void saveRecord(View v){
+        EditText editTextPhone, editTextName, editTextEmail;
+
+        editTextPhone = (EditText)findViewById(R.id.editTextPhone);
+        editTextName = (EditText)findViewById(R.id.editTextName);
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+
+        UserRecord userRecord = new UserRecord();
+        userRecord.setPhone(editTextPhone.getText().toString());
+        userRecord.setName(editTextName.getText().toString());
+        userRecord.setEmail(editTextEmail.getText().toString());
+
+        UserSQLHelper userDataSource = new UserSQLHelper(this);
+        userDataSource.insertUser(userRecord);
+
+        this.finish(); //Terminate this Activity
+    }
+
 }
--- app/src/main/java/my/edu/tarc/lab43sqlite/MainActivity.java
+++ app/src/main/java/my/edu/tarc/lab43sqlite/MainActivity.java
@@ -9,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView listViewRecords;
+    UserSQLHelper userSQLHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        listViewRecords = (ListView)findViewById(R.id.listViewRecords);
+        listViewRecords.setOnItemClickListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdd);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,4 +60,32 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "View: " + view.getTag(0).toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateList();
+    }
+
+    private void updateList() {
+        //Retrieve records from SQLite
+        userSQLHelper = new UserSQLHelper(this);
+
+        final List<UserRecord> values = userSQLHelper.getAllUsers();
+
+        if(values.isEmpty()){
+            Toast.makeText(getApplicationContext(), getString(R.string.no_record_message), Toast.LENGTH_SHORT).show();
+        }
+
+        UserRecordAdapter adapter = new UserRecordAdapter(this, R.layout.user_record, values);
+
+        //Link adapter to ListView
+        listViewRecords.setAdapter(adapter);
+    }
+
 }
