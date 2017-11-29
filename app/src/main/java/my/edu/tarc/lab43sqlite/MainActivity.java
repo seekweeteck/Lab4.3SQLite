a4056ca6eb5bcb1d4f5783dd23a6729c635079a5
seekweeteck <seekt@acd.tarc.edu.my>
2017-11-29 23:01:48 +0800

    Initial

--- app/src/main/java/my/edu/tarc/lab43sqlite/MainActivity.java
+++ app/src/main/java/my/edu/tarc/lab43sqlite/MainActivity.java
@@ -3,12 +3,11 @@ package my.edu.tarc.lab43sqlite;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -63,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), "View: " + view.getTag(0).toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Position: " + position, Toast.LENGTH_SHORT).show();
     }
 
     @Override
